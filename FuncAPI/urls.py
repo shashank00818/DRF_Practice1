42e09ee885c19a4427ed0ee0f234c07a15ceac34
shashank00818 <shashank00818@gmail.com>
2023-08-09 22:59:07 +0530

    Simple jwt authentication start

--- FuncAPI/urls.py
+++ FuncAPI/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from api import views
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('studentapi/', views.StudentAPI.as_view()),
     path('studentapi/<int:pk>', views.StudentAPI.as_view()),
+    path('gettoken/', TokenObtainPairView.as_view()),
+    path('refreshtoken/', TokenRefreshView.as_view()),
+    path('verifytoken/', TokenVerifyView.as_view()),
 ]
